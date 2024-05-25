37c8a5ed03904d521ae43af57b89d393195762e3
Abhishek Singh <abhinav210702@gmail.com>
2024-05-25 10:56:39 +0530

    Changed color of Footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,23 +1,23 @@
 import Image from 'next/image';
 import React from 'react'
 import { MdArrowOutward } from "react-icons/md";
-import logo from "/public/assets/logo-footer.webp";
+import logo from "/public/assets/logo.png";
 
 const Footer = () => {
   return (
-    <footer className="p-sectionPadding h-screen ">
+    <footer className="p-sectionPadding h-screen bg-secondary text-white rounded-t-3xl">
       <div className='grid grid-cols-12 items-end'>
         <div className='col-span-9'>
-          <h1 className='text-8xl font-[melodrama] uppercase w-3/4'>Unleash your inner glow with every visit.</h1>
+          <h1 className='xl:text-8xl font-[melodrama] uppercase w-3/4'>Unleash your inner glow with every visit.</h1>
         </div>
         <div className='col-span-3 flex justify-end items-start flex-col gap-8'>
           <h2 className='text-[nunito] text-lg w-3/4'>Ready to feel your best? Click here to book your appointment now!</h2>
-          <button className='bg-secondary text-[nunito] text-primary font-bold rounded-xl w-full uppercase py-2 flex justify-center items-center text-xl'>Book your appointment <MdArrowOutward /></button>
+          <button className='bg-primary text-[nunito] text-secondary font-bold w-full uppercase py-4 flex justify-center items-center text-xl'>Book your appointment <MdArrowOutward /></button>
         </div>
       </div>
-      <div className='mt-24 grid grid-cols-12 border-b-4'>
-        <div className='col-span-4'>
-          <Image src={logo} alt='logo' className='opacity-20' width={500} height={500}/>
+      <div className='mt-24 grid grid-cols-12 border-b-2'>
+        <div className='col-span-4 mb-4'>
+          <Image src={logo} alt='logo' className='opacity-100' width={500} height={500}/>
         </div>
         <div className='col-span-4 flex justify-center items-end flex-col'>
           <h1 className='font-[melodrama] uppercase text-3xl mb-2'>Harbour town</h1>
